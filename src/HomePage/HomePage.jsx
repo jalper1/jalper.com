7bdeb54c859ab3e9e2f2329d2799a434c0f289e1
Jordan <jalper3@wisc.edu>
2023-12-14 14:37:03 -0600

    styled homepage

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,12 +1,32 @@
-import React, { useEffect } from "react";
+// HomePage.jsx
+import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import "./homePage.css";
 
 function HomePage() {
+  const [jobTitle, setJobTitle] = useState("Software Engineer");
+  const jobTitles = [
+    "Game Designer",
+    "Problem Solver",
+    "Team Player",
+    "Critical Thinker",
+    "Creative Innovater",
+    "Motivated Learner",
+    "Software Engineer",
+  ];
+  let titleIndex = 0;
+
   useEffect(() => {
     document.body.classList.add("no-scroll");
+
+    const typingInterval = setInterval(() => {
+      setJobTitle(jobTitles[titleIndex]);
+      titleIndex = (titleIndex + 1) % jobTitles.length;
+    }, 3000);
+
     return () => {
       document.body.classList.remove("no-scroll");
+      clearInterval(typingInterval);
     };
   }, []);
 
@@ -15,9 +35,22 @@ function HomePage() {
       <NavBar />
       <div className="welcome-container">
         <div className="welcome-text">
-          <h1>Software Engineer</h1>
-          <p>and</p>
-          <h1>Game Designer</h1>
+          <h1 className="animated-text-header">I'm a</h1>
+          <h1 className="animated-text">{jobTitle.trim()}</h1>
+          <div className="welcome-message">
+            <p>
+              Thanks for dropping by! Explore my work history, projects, and
+              contact information.
+            </p>
+          </div>
+          <button
+            className="explore-button"
+            onClick={() => {
+              window.location.href = "/projectpage";
+            }}
+          >
+            Explore My Work
+          </button>
         </div>
       </div>
     </div>
