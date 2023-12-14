079190eddc9679ea48228697671a48cf13c4e25b
Jordan <jalper3@wisc.edu>
2023-12-14 16:15:34 -0600

    contact form

--- src/ContactPage/ContactPage.jsx
+++ src/ContactPage/ContactPage.jsx
@@ -1,15 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import "./contactPage.css";
 
 function ContactPage() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  function addToSubject() {
+    setName("New Contact Form from " + name);
+  }
+
   return (
     <>
       <NavBar />
       <div className="contact-container">
         <div className="contact-text">
           <h1>Contact Me!</h1>
+          <p>
+            I'm always open to new ideas or just to chat. Feel free to reach out
+            with anything!
+          </p>
         </div>
+        <form
+          className="email-form"
+          action="mailto:alperjordan@gmail.com"
+          method="get"
+          enctype="text/plain"
+        >
+          <label className="label-email">Name:</label>
+          <input
+            type="text"
+            name="subject"
+            className="email-email"
+            placeholder="Email"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <label className="label-email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            className="email-email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <label className="label-body">Message:</label>
+          <textarea
+            name="body"
+            className="email-body"
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <div className="email-submit-container">
+            <input
+              className="email-submit"
+              type="submit"
+              value="Send Email"
+              onClick={addToSubject}
+            />
+          </div>
+        </form>
       </div>
     </>
   );
