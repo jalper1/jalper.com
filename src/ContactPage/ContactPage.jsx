c7cee068192cad7fd9e7b0c3f377654e10f96b09
Jordan <jalper3@wisc.edu>
2023-12-15 16:48:54 -0600

    fixed mobile contact form

--- src/ContactPage/ContactPage.jsx
+++ src/ContactPage/ContactPage.jsx
@@ -7,10 +7,15 @@ function ContactPage() {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
   const [subject, setSubject] = useState("");
+  const [visible, setVisible] = useState(true);
 
   function addToSubject() {
     setSubject("New Contact Form from " + name);
   }
+
+  useEffect(() => {
+    window.innerWidth <= 600 ? setVisible(false) : setVisible(true);
+  }, [window]);
 
   return (
     <>
@@ -24,53 +29,71 @@ function ContactPage() {
             with anything!
           </p>
         </div>
-        <form
-          className="email-form"
-          action="mailto:alperjordan@gmail.com"
-          method="get"
-          encType="text/plain"
-        >
-          <label className="label-email">Name:</label>
-          <input
-            hidden={true}
-            type="text"
-            name="subject"
-            className="email-email"
-            placeholder="Email"
-            value={subject}
-          ></input>
-          <input
-            className="email-email"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          ></input>
-          <label className="label-email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            className="email-email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-          <label className="label-body">Message:</label>
-          <textarea
-            name="body"
-            className="email-body"
-            placeholder="Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-          <div className="email-submit-container">
+        {visible ? (
+          <form
+            className="email-form"
+            action="mailto:alperjordan@gmail.com"
+            method="get"
+            encType="text/plain"
+          >
+            <label className="label-email">Name:</label>
             <input
-              className="email-submit"
-              type="submit"
-              value="Send Email"
-              onClick={addToSubject}
-            />
-          </div>
-        </form>
+              hidden={true}
+              type="text"
+              name="subject"
+              className="email-email"
+              placeholder="Email"
+              value={subject}
+            ></input>
+            <input
+              className="email-email"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <label className="label-email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="email-email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <label className="label-body">Message:</label>
+            <textarea
+              name="body"
+              className="email-body"
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <div className="email-submit-container">
+              <input
+                className="email-submit"
+                type="submit"
+                value="Send Email"
+                onClick={addToSubject}
+              />
+            </div>
+          </form>
+        ) : (
+          <form
+            className="email-form"
+            action="mailto:alperjordan@gmail.com"
+            method="get"
+            encType="text/plain"
+          >
+            <div className="email-submit-container">
+              <input
+                className="email-submit"
+                type="submit"
+                value="Send Email"
+                onClick={addToSubject}
+              />
+            </div>
+          </form>
+        )}
       </div>
     </>
   );
